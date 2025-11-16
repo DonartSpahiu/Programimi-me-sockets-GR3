4b04a055a2ce4c7db16190ed318398b4a5244448
dajvazi <donart.ajvazi@hotmail.com>
2025-11-16 15:15:58 +0100

    Adding client's commands

--- client.php
+++ client.php
@@ -27,6 +27,31 @@ if ($pergjigje === 'po') {
     send_and_receive($socket, "PERSHENDETJE", $server_ip, $server_port);
 }
 
+while (true) {
+    $msg = readline("Jep komanden ose mesazhin: ");
+    if ($msg === 'exit') break;
 
+    if (str_starts_with($msg, "/upload ")) {
+        [$cmd, $filename] = explode(" ", $msg, 2);
+        if (!file_exists($filename)) { echo "Skedari nuk ekziston.\n"; continue; }
+        $encoded = base64_encode(file_get_contents($filename));
+        $msg = "/upload " . basename($filename) . " " . $encoded;
+    }
+    elseif (str_starts_with($msg, "/download ")) {
+        socket_sendto($socket, $msg, strlen($msg), 0, $server_ip, $server_port);
+        $buf = null; $from = null; $fp = null;
+        $bytes = @socket_recvfrom($socket, $buf, 65535, 0, $from, $fp);
+        if ($bytes === false) { echo "Asnjë përgjigje.\n"; continue; }
+        $filename = substr($msg, 10);
+        $decoded = base64_decode($buf);
+        file_put_contents("downloaded_$filename", $decoded);
+        echo "Skedari u shkarkua si 'downloaded_$filename'.\n";
+        continue;
+    }
 
+    send_and_receive($socket, $msg, $server_ip, $server_port);
+}
+
+socket_close($socket);
+echo "Lidhja u mbyll.\n";
 ?>
